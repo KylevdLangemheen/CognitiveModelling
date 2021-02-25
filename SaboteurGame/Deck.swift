e2d68c8f23d3878dbd6f4c56ea3ccd1d91ee7ff0
Alex Charisis <SCAlex91@users.noreply.github.com>
2021-02-25 13:50:47 +0100

    Deck initialisation

--- SaboteurGame/Deck.swift
+++ SaboteurGame/Deck.swift
@@ -10,12 +10,29 @@ import Foundation
 struct Deck {
     
     var deck = Array<Card>()
-
     
-    struct Card {
+    let pathCards: Int = 44
+    let goldenNuggetCards: Int = 28
+    let actionCards: Int = 27
+    
+    
+//      let goldDiggers = 2
+//      let saboteurs = 1
+    
+    init(){
+        deck = Array(repeating: Card(isFaceUp: true, cardType: 0, cardContent: "⛏", id: 0), count: pathCards)
+    }
+
+//    func returnIds(pathCards: Int) -> Array<Int> {
+//        var pathCardsIdx = Array(0...pathCards)
+//        pathCardsIdx.shuffle()
+//        return pathCardsIdx
+//    }
+            
+    struct Card: Identifiable {
         var isFaceUp: Bool = true
         var cardType: Int = 0
-        var cardConent: String
+        var cardContent: String
         var id: Int
     }
 }
