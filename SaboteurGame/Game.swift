4edfc1e9f0b9c9661d7af82b13161d2c324cf717
H.B.R. van de Langemheen <s2985489@ai178133.ai.rug.nl>
2021-03-18 13:29:29 +0100

    Implemented most of the logic for computerPlay

--- SaboteurGame/Game.swift
+++ SaboteurGame/Game.swift
@@ -60,6 +60,48 @@ struct Game {
         possiblePlays.shuffle()
         if possiblePlays.count != 0 {
             computer.playerStatus = .placingCard
+            var playerRoles: [Int: (String, Double)] = [:]
+            let playerMap: [Int: String] = [
+                0: "one",
+                1: "zero"
+            ]
+            var activation = 0.4
+            for player in players.players {
+                if let playerno = playerMap[player.id] {
+                    //TODO: skip if playerno is "zero"
+                    //model.modifyLastAction(slot: "playerno", value: playerno)
+                    //(let role, let activation) = model.lastAction(slot: "role")
+                    //playerRoles[player.id] = (role, activation)
+                    playerRoles[player.id] = (playerno, activation)
+                    activation += 0.2
+                }
+            }
+            var sortedKeyValues = Array(playerRoles).sorted(by: {$0.value.1 > $1.value.1})
+            var toRemove: Array<Int> = []
+            for (index, (key, value)) in sortedKeyValues {
+                if key == "unknown" {
+                    toRemove.append(index)
+                }
+            }
+            for i in toRemove {
+                sortedKeyValues.remove(at: i)
+            }
+            for (key, (role, activation)) in sortedKeyValues {
+                //TODO: find a possible action card to play:
+                //Go over each possibleActions
+                    //check if the role matches the player role
+                        //possible action has to be helpful to that player
+                    //else
+                        //possible action has to be unhelpful to that player
+            }
+            //TODO: if action has been found, play it. else, if not blocked:
+            //Sort possible players by coop value (desc for miner, asc for saboteur)
+            //play most coop and value > 3 if miner
+            //play most uncoop and value < 3 if saboteur
+            //if nothing found or if blocked:
+            //find cards to swap
+            //swap worst cards, up to 3.
+            
             placeCard(card: possiblePlays[0].card, cell: possiblePlays[0].cell)
         } else {
             for card in computer.hand {
